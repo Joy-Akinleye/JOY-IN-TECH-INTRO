60e6cdc70edcd3e8f0575181086d39eef503beee
MideJoy Akinleye <joyintech50@gmail.com>
2025-03-14 23:19:41 +0100

    sidebar menu

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuBtn = document.getElementById("navbar__menu-btn");
+const closeBtn = document.getElementById("navbar__close-btn");
+const sidebar = document.querySelector('.sidebar')
+
+
+const showSidebar = (event) => {
+    // event.preventDefault(); // prevent unintended closing
+    sidebar.classList.add("show");
+}
+
+const hideSidebar = (event) => {
+    sidebar.classList.remove("show");
+}
+
+menuBtn.addEventListener('click', showSidebar);
+closeBtn.addEventListener('click', hideSidebar);
