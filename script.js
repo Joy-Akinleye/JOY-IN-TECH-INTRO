13091fc4f93eb5dad11ee7a9a5aae82f9c64130f
MideJoy Akinleye <joyintech50@gmail.com>
2025-03-27 12:29:35 +0100

    responsiveness

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ const sidebar = document.querySelector('.sidebar')
 
 
 const showSidebar = (event) => {
-    // event.preventDefault(); // prevent unintended closing
+    event.preventDefault(); // prevent unintended closing
     sidebar.classList.add("show");
 }
 
 const hideSidebar = (event) => {
+    event.preventDefault();
     sidebar.classList.remove("show");
 }
 
 menuBtn.addEventListener('click', showSidebar);
 closeBtn.addEventListener('click', hideSidebar);
+document.addEventListener("DOMContentLoaded", hideSidebar);
